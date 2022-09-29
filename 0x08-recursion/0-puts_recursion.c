8396fb0d9de3b4b028bb8f34067d9c3160e557cd
ToHLz <petrapatrick127@gmail.com>
2022-09-29 11:11:10 -0700

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - The function that will a string following a newline
+ * *s: The string that will be printed
+ *
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	printf("%c", *s);
+	puts_recursion(*s + 1);
+}
